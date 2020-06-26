a114018ddef1434f2934911c7f8d869b25c744cf
Dara Vasconcelos <dara@MacBook-Pro-de-Dara.local>
2020-06-25 23:02:19 -0300

    personagem andando e bg continuo

--- Lendario/MaeDaguaScene.swift
+++ Lendario/MaeDaguaScene.swift
@@ -10,30 +10,60 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
+
 class MaeDaguaScene: SKScene {
     private var character = SKSpriteNode()
     private var charWalkingFrames: [SKTexture] = []
     let cam = SKCameraNode()
+    var background1 = SKSpriteNode()
+    var background2 = SKSpriteNode()
     
+    
+
     override func didMove(to view: SKView) {
         print("Entrou na cena da Lenda da Mãe D'água")
         self.anchorPoint = .zero
         self.camera = cam
         buildChar()
-        animateChar()
-        self.addBackground()
+       // animateChar()
+        
+        //background setup
+        background1 = SKSpriteNode(imageNamed: "backgroundRio")
+        background1.anchorPoint = CGPoint.zero
+        background1.position = CGPoint.zero
+        background1.zPosition = -1
+        self.addChild(background1)
+        
+        //background setup
+        background2 = SKSpriteNode(imageNamed: "backgroundRio2")
+        background2.anchorPoint = CGPoint.zero
+        background2.position = CGPoint(x: background1.frame.width, y: 0)
+        background2.zPosition = -1
+        self.addChild(background2)
+
+
     }
     
+    override func update(_ currentTime: TimeInterval) {
+        updateBackground()
+    }
+    
+    func updateBackground(){
+        if(cam.position.x > background1.position.x + background1.size.width + 400){
+            background1.position = CGPoint(x: background2.position.x+background2.size.width, y: background1.position.y)
+        }
+        
+        if(cam.position.x > background2.position.x + background2.size.width + 400){
+            background2.position = CGPoint(x: background1.position.x+background1.size.width, y: background2.position.y)
+        }
+        
+        
+    }
+
     override func didSimulatePhysics() {
         self.camera!.position = character.position
     }
-    
-    func addBackground(){
-        let bg = SKSpriteNode(imageNamed: "horizonteBg")
-        bg.position = CGPoint(x: 250, y: 250)
-        self.addChild(bg)
-    }
-    
+
     func buildChar() {
       let charAnimatedAtlas = SKTextureAtlas(named: "character")
       var walkFrames: [SKTexture] = []
@@ -44,14 +74,15 @@ class MaeDaguaScene: SKScene {
         walkFrames.append(charAnimatedAtlas.textureNamed(charTextureName))
       }
       charWalkingFrames = walkFrames
-        
+
         let firstFrameTexture = charWalkingFrames[0]
         character = SKSpriteNode(texture: firstFrameTexture)
-        character.position = CGPoint(x: frame.midX, y: frame.midY)
+        character.position = CGPoint(x: frame.midX, y: frame.midY + 20)
+        character.zPosition = 1
         addChild(character)
-        
+
     }
-    
+
     func animateChar() {
       character.run(SKAction.repeatForever(
         SKAction.animate(with: charWalkingFrames,
@@ -60,7 +91,7 @@ class MaeDaguaScene: SKScene {
                          restore: true)),
         withKey:"walkingInPlaceChar")
     }
-    
+
     func charMoveEnded() {
       character.removeAllActions()
     }
@@ -70,20 +101,21 @@ class MaeDaguaScene: SKScene {
         let location = touch.location(in: self)
         moveChar(location: location)
     }
+    
     func moveChar(location: CGPoint) {
       // 1
       var multiplierForDirection: CGFloat
-      
+
       // 2
       let charSpeed = frame.size.width / 3.0
-      
+
       // 3
       let moveDifference = CGPoint(x: location.x - character.position.x, y: location.y - character.position.y)
       let distanceToMove = sqrt(moveDifference.x * moveDifference.x + moveDifference.y * moveDifference.y)
-      
+
       // 4
       let moveDuration = distanceToMove / charSpeed
-      
+
       // 5
       if moveDifference.x < 0 {
         multiplierForDirection = -1.0
@@ -91,7 +123,7 @@ class MaeDaguaScene: SKScene {
         multiplierForDirection = 1.0
       }
       character.xScale = abs(character.xScale) * multiplierForDirection
-        
+
         // 1
         if character.action(forKey: "walkingInPlaceChar") == nil {
           // if legs are not moving, start them
@@ -99,7 +131,8 @@ class MaeDaguaScene: SKScene {
         }
 
         // 2
-        let moveAction = SKAction.move(to: location, duration:(TimeInterval(moveDuration)))
+        //let moveAction = SKAction.move(to: location, duration:(TimeInterval(moveDuration)))
+        let moveAction = SKAction.moveTo(x: location.x, duration: (TimeInterval(moveDuration)))
 
         // 3
         let doneAction = SKAction.run({ [weak self] in
@@ -110,6 +143,7 @@ class MaeDaguaScene: SKScene {
         let moveActionWithDone = SKAction.sequence([moveAction, doneAction])
         character.run(moveActionWithDone, withKey:"charMoving")
     }
-    
+
 
 }
+
