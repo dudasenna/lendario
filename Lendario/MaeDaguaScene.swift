d5261d4283f6630c71778c8b2a7e4e21af269ba4
Jéssica Amaral <jaaao@cin.ufpe.br>
2020-06-29 16:00:42 -0300

    Define limitações para o movimento da personagem principal e muda a localização do background1

--- Lendario/MaeDaguaScene.swift
+++ Lendario/MaeDaguaScene.swift
@@ -22,38 +22,44 @@ class MaeDaguaScene: SKScene {
     
     var charVelocity: CGFloat = 0
     
+    var inicialPos: CGPoint!
+    var livePos: CGPoint!
+    
     override func didMove(to view: SKView) {
-        buildChar()
+        self.anchorPoint = .zero
         
-        cam.position = character.position
-        self.addChild(cam)
+        //atribui a câmera
         self.camera = cam
+        
+        //Posiciona a personagem principal na c3ena
+        buildChar()
         
         //background setup
         background1 = SKSpriteNode(imageNamed: "BackgroundDia")
         background1.anchorPoint = self.anchorPoint
-        background1.position = CGPoint.zero
-        background1.size = CGSize(width: background1.frame.width, height: size.height * 1.05)
-        print(background1.size)
+        background1.position = CGPoint(x: -size.width * 2, y: .zero)
+        background1.size = CGSize(width: size.width * 6, height: size.height * 1.05)
         background1.zPosition = -1
         self.addChild(background1)
+        
         
         //background setup
         background2 = SKSpriteNode(imageNamed: "BackgroundNoite")
         background2.anchorPoint = self.anchorPoint
         background2.position = CGPoint(x: background1.position.x + background1.frame.width, y: 0)
-        background2.size = CGSize(width: background2.frame.width, height: background1.frame.height)
+        background2.size = CGSize(width: size.width * 6, height: background1.frame.height)
         background2.zPosition = -1
         self.addChild(background2)
     }
     
     override func update(_ currentTime: TimeInterval) {
-        cam.position = character.position
+        //Define a velocidade da personagem (andando pra frente - 200 -, pra trás - -200 - ou parada)
         character.physicsBody?.velocity.dx = charVelocity
         //updateBackground()
     }
     
-    private func updateBackground(){
+    /*private func updateBackground(){
+        
         if(cam.position.x > background1.position.x + background1.size.width * 1.5){
             if charVelocity > 0 {
                 background1.position = CGPoint(x: background2.position.x + background2.size.width, y: background1.position.y)
@@ -65,36 +71,44 @@ class MaeDaguaScene: SKScene {
                  background2.position = CGPoint(x: background1.position.x + background1.size.width, y: background2.position.y)
             }
         }
-    }
-    
-    /*override func didSimulatePhysics() {
-        cam.position = character.position
     }*/
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        let touch = touches.first!
-        let location = touch.location(in: self)
-        var multiplierForDirection: CGFloat
-        
-        character.isPaused = false
-        animateChar()
-        
-        //Define a distância que o personagem irá percorrer, baseada no clique
-        let moveDifference = CGFloat(location.x - character.position.x)
-        
-        //Checa se o clique foi à esquerda ou à direita da posição atual do personagem
-        if moveDifference < 0 {
-            charVelocity = -200
-            multiplierForDirection = -1
-        } else {
-            charVelocity = 200
-            multiplierForDirection = 1
-        }
-        
-        character.xScale = abs(character.xScale) * multiplierForDirection
+    //Coloca a câmera para "seguir a personagem pela cena"
+    override func didSimulatePhysics() {
+        self.camera!.position = character.position
     }
     
+    //Chamada quando um toque na tela é identificado
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //Salva localização do primeiro toque
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        
+        //A personagem só se movimenta pra ambos os lados se sua posição for maior do que a inicial, se for menor, ela só pode ir para frente
+        if character.position.x > inicialPos.x || location.x > character.position.x{
+            var multiplierForDirection: CGFloat
+            
+            character.isPaused = false
+            animateChar()
+            
+            //Define a distância que o personagem irá percorrer, baseada no clique
+            let moveDifference = CGFloat(location.x - character.position.x)
+            
+            //Checa se o clique foi à esquerda ou à direita da posição atual do personagem
+            if moveDifference < 0 {
+                charVelocity = -200
+                multiplierForDirection = -1
+            } else {
+                charVelocity = 200
+                multiplierForDirection = 1
+            }
+            
+            character.xScale = abs(character.xScale) * multiplierForDirection
+        }
+    }
+    
+    //Ao retirar o dedo da tela, o movimento para
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         charVelocity = 0
         charMoveEnded()
@@ -113,21 +127,22 @@ class MaeDaguaScene: SKScene {
         
         let firstFrameTexture = charWalkingFrames[0]
         character = SKSpriteNode(texture: firstFrameTexture)
-        character.position = CGPoint(x: background1.position.x + character.size.width, y: frame.midY + 20)
+        
+        character.position = CGPoint(x: frame.minX, y: frame.midY + 20)
         character.zPosition = 1
+        inicialPos = character.position
+        
         character.physicsBody = SKPhysicsBody(rectangleOf: character.size)
         character.physicsBody?.affectedByGravity = false
+        
         self.addChild(character)
         
     }
     
     private func animateChar() {
-        character.run(SKAction.repeatForever(
-            SKAction.animate(with: charWalkingFrames,
-                             timePerFrame: 0.2,
-                             resize: false,
-                             restore: false)),
-                      withKey:"walkingInPlaceChar")
+        let walkAction = SKAction.animate(with: charWalkingFrames, timePerFrame: 0.3)
+        let charAction = SKAction.repeatForever(walkAction)
+        character.run(charAction)
     }
     
     private func charMoveEnded() {
